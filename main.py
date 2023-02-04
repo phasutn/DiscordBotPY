9469de9f497dc1295d393cc57b203c1ca807d290
phasutn <ssam02537@gmail.com>
2023-02-05 03:02:02 +0700

    Version 1

---
+++ main.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands
+import asyncio
+import os
+
+TOKEN = 'INSERT TOKEN HERE'
+intents = discord.Intents.all()
+intents.message_content = True
+intents.members = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+
+@commands.command(name='greet')
+async def greet(ctx):
+    await ctx.send("HELLO")
+
+
+async def load():
+    for filename in os.listdir('cogs'):
+        if filename.endswith('.py'):
+            await bot.load_extension(f'cogs.{filename[:-3]}')
+
+
+async def main():
+    await load()
+    bot.add_command(greet)
+
+
+if __name__ == '__main__':
+    # discord.opus.load_opus("./libopus.so.0.8.0")
+    asyncio.run(main())
+    bot.run(TOKEN)
