92829c10b49e024f826de1a40763bf1a5bb8dd56
phasutn <ssam02537@gmail.com>
2023-09-19 21:59:53 +0700

    I removed the API key

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import asyncio
 import os
 
-TOKEN = 'MTA3MTUyNTA4NjQ4NzY2MjYxMg.GLxWeD.TcRi2-9fcpMR7oOQwezXi8EeHbPxHW72QYff60' # DISCORD API TOKEN HERE
+TOKEN = '' # DISCORD API TOKEN HERE
 intents = discord.Intents.all()
 intents.message_content = True
 intents.members = True
